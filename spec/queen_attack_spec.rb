97d7db0594a87afeea978f0e72e3cba0b6e39d23
Chase-Stokes <chasehstokes@gmail.com>
2021-12-06 13:53:55 -0800

    5th test passed
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>

--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -14,4 +14,7 @@ describe ('#queen_attack?') do
   it('is true if the coordinates are horizontally in line with each other') do
     expect(queen_attack?(([1,1]), ([2,2]))).to(eq(true))
   end
+  it('is true if the coordinates are horizontally in line with each other') do
+    expect(queen_attack?(([2,2]), ([1,1]))).to(eq(true))
+  end
 end
